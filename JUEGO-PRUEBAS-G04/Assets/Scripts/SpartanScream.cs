3e4c8f93e95e0d7265920f74fc82633395c66da6
ssimoanto <simoanto@ucm.es>
2024-05-17 18:41:43 +0200

    espartano

---
+++ JUEGO-PRUEBAS-G04/Assets/Scripts/SpartanScream.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SonidoPersonaje : MonoBehaviour
+{
+    public AudioClip sonido; // Arrastra aquí el archivo de sonido desde el Inspector de Unity
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null) // Si no hay AudioSource en el objeto, lo añadimos
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.clip = sonido;
+        InvokeRepeating("ReproducirSonido", 0, 5); // Inicia la repetición del sonido cada 2 segundos
+    }
+
+    void ReproducirSonido()
+    {
+        audioSource.Play();
+    }
+}
