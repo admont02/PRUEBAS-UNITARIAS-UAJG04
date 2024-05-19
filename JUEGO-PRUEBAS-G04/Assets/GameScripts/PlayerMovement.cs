8123ea8579da03a1ba7d6c8c8d6bc7760d7d686d
asegar01 <asegar01@ucm.es>
2024-05-19 11:05:34 +0200

    Movimiento cámara y rotación correcta

--- JUEGO-PRUEBAS-G04/Assets/GameScripts/PlayerMovement.cs
+++ JUEGO-PRUEBAS-G04/Assets/GameScripts/PlayerMovement.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float rotationSpeed = 5.0f;
+
+    float rotation = 0.0f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // Actualizamos la posición del jugador en cada frame
     void Update()
@@ -13,7 +23,15 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 localMovement = transform.TransformDirection(movement) * speed;
 
-        GetComponent<Rigidbody>().velocity = movement * speed;
+        rb.velocity = new Vector3(localMovement.x, rb.velocity.y, localMovement.z);
+
+
+        if (Input.GetKey(KeyCode.Q)) rotation = -rotationSpeed;
+        else if (Input.GetKey(KeyCode.E)) rotation = rotationSpeed;
+        else rotation = 0.0f;
+
+        transform.Rotate(Vector3.up, rotation * rotationSpeed * Time.deltaTime);
     }
 }
