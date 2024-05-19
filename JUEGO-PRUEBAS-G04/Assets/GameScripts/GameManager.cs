6a6503d3c43fc905ba67a42b1b4ebc941bd50776
ssimoanto <simoanto@ucm.es>
2024-05-19 18:03:31 +0200

    reimportado package

--- JUEGO-PRUEBAS-G04/Assets/GameScripts/GameManager.cs
+++ JUEGO-PRUEBAS-G04/Assets/GameScripts/GameManager.cs
@@ -5,41 +5,41 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    //[SerializeField]
-    //Toggle toggle;  // Referencia al Toggle en la UI
-    //private bool herramientaActiva = true;
+    [SerializeField]
+    Toggle toggle;  // Referencia al Toggle en la UI
+    private bool herramientaActiva = true;
 
-    //void Start()
-    //{
-    //    // Asegurarse de que el Toggle refleja el estado inicial
-    //    toggle.isOn = herramientaActiva;
-    //    // Añadir listener al Toggle
-    //    toggle.onValueChanged.AddListener(OnToggleChanged);
-    //    // Aplicar el estado inicial
-    //    UpdateScriptActivation();
-    //}
+    void Start()
+    {
+        // Asegurarse de que el Toggle refleja el estado inicial
+        toggle.isOn = herramientaActiva;
+        // Añadir listener al Toggle
+        toggle.onValueChanged.AddListener(OnToggleChanged);
+        // Aplicar el estado inicial
+        UpdateScriptActivation();
+    }
 
-    //void OnToggleChanged(bool isOn)
-    //{
-    //    herramientaActiva = isOn;
-    //    UpdateScriptActivation();
-    //}
+    void OnToggleChanged(bool isOn)
+    {
+        herramientaActiva = isOn;
+        UpdateScriptActivation();
+    }
 
-    //void UpdateScriptActivation()
-    //{
-        
-    //    // Encontrar todas las instancias de MyScript en la escena
-    //    Transmitter[] scripts = FindObjectsOfType<Transmitter>();
+    void UpdateScriptActivation()
+    {
 
-    //    // Activar/desactivar cada instancia basado en herramientaActiva
-    //    foreach (var script in scripts)
-    //    {
-    //        script.enabled = herramientaActiva;
-    //    }
-    //    Listener list = FindObjectOfType<Listener>();
-    //    list.enabled = herramientaActiva;
-    //    CanvasSoundController controller = FindObjectOfType<CanvasSoundController>();
-    //    controller.ClearAll();
-    //    controller.enabled = herramientaActiva;
-    //}
+        // Encontrar todas las instancias de MyScript en la escena
+        Transmitter[] scripts = FindObjectsOfType<Transmitter>();
+
+        // Activar/desactivar cada instancia basado en herramientaActiva
+        foreach (var script in scripts)
+        {
+            script.enabled = herramientaActiva;
+        }
+        Receiver list = FindObjectOfType<Receiver>();
+        list.enabled = herramientaActiva;
+        IndicatorController controller = FindObjectOfType<IndicatorController>();
+        controller.ClearAll();
+        controller.enabled = herramientaActiva;
+    }
 }
